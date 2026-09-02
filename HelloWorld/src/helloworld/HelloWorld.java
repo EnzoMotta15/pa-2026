d1a1567db52cd7e8f672837dd75b4dcdbee06f56
unknown <nanat16.0408@gmail.com>
2026-09-01 22:26:46 -0300

    Primeira Aula de Java

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+        System.out.println("João Lindo Saudades"); 
+        System.out.println("3+3");
+        
+        
+        // entrada de valores
+        int Numero1 = 10;
+        int Numero2 = 20;
+        int resultado;
+        
+        // Processamento
+        // Linhas 30 e 31 Soma 
+        resultado = Numero1 + Numero2;
+        System.out.println("O Resultado da Soma é:" + resultado);
+        resultado = Numero1 / Numero2;
+        System.out.println("(O Resultado da Divisão é:" + resultado);
+        resultado = Numero1 * Numero2;
+        System.out.println("O Resultado da Multiplicação é:" + resultado);
+        resultado = Numero1 - Numero2;
+        System.out.println("O REsultado da Subtração é:" + resultado);
+        
+        // Saida de valores
+        
+        System.out.println(resultado);
+        
+        // TODO code application logic here
+    }
+    
+}
